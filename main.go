ff75b5c898964335827e03e11349c99f11ffb449
Timo G <Timo.Guelich@weidmueller.com>
2026-08-20 09:55:10 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"runtime/debug"
 )
 
-func main() {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				fmt.Printf("Commit: %s\n", setting.Value)
-			}
+// The import path of your module as defined in go.mod
+const modulePath = "github.com/TimoG-WI/go-version-demo"
+
+// Version returns the version of this library embedded in the binary.
+func Version() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+
+	// 1. If consumed as a module dependency in another application:
+	for _, dep := range info.Deps {
+		if dep.Path == modulePath {
+			return dep.Version
 		}
 	}
+
+	// 2. If running directly from inside this module (e.g. tests or main binary):
+	if info.Main.Path == modulePath && info.Main.Version != "" {
+		return info.Main.Version
+	}
+
+	return "unknown"
+}
+
+func main(){
+	fmt.Println("You are running version:", Version())
 }
